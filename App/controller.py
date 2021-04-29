42cd4976f3990e832933cf09d9d156260f77783d
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-04-28 20:52:39 -0500

    cambios en Controller

--- App/controller.py
+++ App/controller.py
@@ -1,9 +1,9 @@
 ﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
+ * Copyright 2021, Departamento de Sistemas y Computación,
  * Universidad de Los Andes
  *
  *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ * Desarrollado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
  *
  *
  * This program is free software: you can redistribute it and/or modify
@@ -13,26 +13,88 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ * along withthis program. If not, see <http://www.gnu.org/licenses/>.
  """
 
 import config as cf
 import model
 import csv
 
-
 """
-El controlador se encarga de mediar entre la vista y el modelo.
+El controlador se encarga de mediar entre la vista y el modelo
 """
 
-# Inicialización del Catálogo de libros
+# Inicialización del analizador de eventos
+
+def initAnalyzer():
+    """
+    Llama la función de inicialización del analizador
+    """
+    analyzer = model.newAnalyzer()
+    return analyzer
 
 # Funciones para la carga de datos
 
-# Funciones de ordenamiento
+def loadData(analyzer):
+    """
+    Carga los datos de los archivos csv en las estructuras
+    de datos
+    """
+    loadEvents(analyzer)
 
-# Funciones de consulta sobre el catálogo
+def loadEvents(analyzer):
+    """
+    Carga los eventos del archivo csv. Por cada evento se indica al
+    modelo que debe adicionarlo al analizador
+    """
+    eventsfile = cf.data_dir + 'subsamples-small/context_content_features-small.csv'
+    input_file = csv.DictReader(open(eventsfile, encoding='utf-8'))
+    for event in input_file:
+        model.addEvent(analyzer, event)
+
+def loadHashtags(analyzer):
+    """
+    Carga las etiquetas del archivo csv. Por cada etiqueta se indica al
+    modelo que debe adicionarla al analizador
+    """
+    tagsfile = cf.data_dir + 'subsamples-small/user_track_hashtag_timestamp-small.csv'
+    input_file = csv.DictReader(open(tagsfile, encoding='utf-8'))
+    for hashtag in input_file:
+        model.addHashtag(analyzer, hashtag)
+
+def loadSentimentValues(analyzer):
+    """
+    Carga los valores de sentimientos del archivo csv. Por cada valor se
+    indica al modelo que debe adicionarlo al analizador
+    """
+    sentimentsfile = cf.data_dir + 'subsamples-samll/sentiment_values.csv'
+    input_file = csv.DictReader(open(sentimentsfile, encoding='utf-8'))
+    for sentimentvalue in input_file:
+        model.addSentimentValue(analyzer, sentimentvalue)
+
+# Funciones de consulta
+
+def eventsSize(analyzer):
+    """
+    Retorna el número de eventos cargados en el
+    analizador
+    """
+    return model.eventsSize(analyzer)
+
+def tracksSize(analyzer):
+    """
+    Retorna el número de pistas cargadas en el
+    analizador
+    """
+    return model.tracksSize(analyzer)
+
+def artistsSize(analyzer):
+    """
+    Retorna el número de artistas cargados en el
+    analizador
+    """
+    return model.artistsSize(analyzer)
